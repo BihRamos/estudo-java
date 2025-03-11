0a164d701e07e994f70e94ec5c3146df7aac7c72
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-11 10:24:15 -0300

    Update Aula2.java

--- Aula2.java
+++ Aula2.java
@@ -1,18 +1,21 @@
+//Curso: DS-ETE. Aula ministrada:Prof.Joerllys Sérgio//
+//Caixa de texto solicitando nome, idade e altura//
+
 package praticando2;
 import javax.swing.*;
 public class Praticando2 {
 
     public static void main(String[] args) {
         
-        String nome;
-        int idade=0;
-        double altura=0;
+        String nome; //variavel nome//
+        int idade=0; //variavel idade//
+        double altura=0; //variavel altura//
         
-        nome= JOptionPane.showInputDialog("Informe o seu nome:");
-        idade=Integer.parseInt(JOptionPane.showInputDialog("Informe sua idade"));
-        altura=Double.parseDouble(JOptionPane.showInputDialog("Informe sua altura"));
+        nome= JOptionPane.showInputDialog("Informe o seu nome:"); //caixa de texto soliciando nome//
+        idade=Integer.parseInt(JOptionPane.showInputDialog("Informe sua idade"));   //caixa de texto soliciando idade//
+        altura=Double.parseDouble(JOptionPane.showInputDialog("Informe sua altura"));   //caixa de texto soliciando altura//
         
-        JOptionPane.showMessageDialog(null,"Seu nome :"+ nome+"\nSua idade é:"+idade+"\nSua altura é:"+altura);
+        JOptionPane.showMessageDialog(null,"Seu nome :"+ nome+"\nSua idade é:"+idade+"\nSua altura é:"+altura); // caixa de texto final com nome,idade e altura//
     }
     
 }
