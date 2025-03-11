784a50f56559e787237ffae3dae800773792f787
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-11 10:18:20 -0300

    Update Aula3.java

--- Aula3.java
+++ Aula3.java
@@ -1,4 +1,5 @@
 //Curso: DS-ETE. Aula ministrada:Prof.Joerllys Sérgio//
+// Solicitar login e senha//
 package aula3;
 import javax.swing.*;
 public class Aula3 {
@@ -8,23 +9,23 @@ public class Aula3 {
         
         String login, senha;
         
-        login = JOptionPane.showInputDialog("Digite seu login:");
-        senha= JOptionPane.showInputDialog("Digite sua senha:");
+        login = JOptionPane.showInputDialog("Digite seu login:"); // Caixa de texto para solicitar login//
+        senha= JOptionPane.showInputDialog("Digite sua senha:"); // Caixa de texto para solicitar senha//
         
         
         
         
-        if (login.equals("admin") && senha.equals("$123#"))
+        if (login.equals("admin") && senha.equals("$123#")) // operação de solicitação de login e senha//
         {
         
-                 JOptionPane.showMessageDialog(null,"Acesso aprovado!");
+                 JOptionPane.showMessageDialog(null,"Acesso aprovado!");//situação após login e senha digitados corretamente//
             }
        else
        
        
        {
                
-       JOptionPane.showMessageDialog(null,"Acesso Negado!");
+       JOptionPane.showMessageDialog(null,"Acesso Negado!"); //situação após login e senha digitados de modo incorreto//
                         }
         
     }
