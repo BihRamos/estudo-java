ade01a3ed53f9b9288dfb2741bfe3e7f677ab8ce
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-11 10:09:13 -0300

    Add files via upload

---
+++ Aula3.java
@@ -0,0 +1,32 @@
+
+package aula3;
+import javax.swing.*;
+public class Aula3 {
+
+    
+    public static void main(String[] args) {
+        
+        String login, senha;
+        
+        login = JOptionPane.showInputDialog("Digite seu login:");
+        senha= JOptionPane.showInputDialog("Digite sua senha:");
+        
+        
+        
+        
+        if (login.equals("admin") && senha.equals("$123#"))
+        {
+        
+                 JOptionPane.showMessageDialog(null,"Acesso aprovado!");
+            }
+       else
+       
+       
+       {
+               
+       JOptionPane.showMessageDialog(null,"Acesso Negado!");
+                        }
+        
+    }
+    
+}
