9ac0ef8dcd6b70442623a09cca1da41ff4b9d88b
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-21 12:01:22 -0300

    Add files via upload

---
+++ Banco2.java
@@ -0,0 +1,54 @@
+/* -O método .equals("1") compara strings.
+
+-O while é uma estrutura de repetição usada para executar um 
+bloco de código enquanto uma condição for verdadeira.
+
+- System.exit(0); serve para finalizar o programa.
+*/
+package banco2;
+ import javax.swing.*;
+public class Banco2 {
+
+    public static void main(String[] args) {
+        
+        String login,nome,senha;
+        double saque, deposito,saldo = 100; // Saldo inicial
+        
+        
+      login = JOptionPane.showInputDialog("Digite seu login:");
+      senha = JOptionPane.showInputDialog("Digite sua senha:");
+      nome = JOptionPane.showInputDialog("Digite seu nome:");
+        
+        JOptionPane.showMessageDialog(null, "Acesso liberado! Bem-vindo, " + nome + "!");
+        
+        while (true) {
+            String opcao = JOptionPane.showInputDialog("1. Ver saldo\n" + "2. Depositar\n" + "3. Sacar\n"+ "4. Sair\n"+ "Escolha uma opção:"
+            );
+            
+           if (opcao.equals("1"))//compararstring// 
+           {
+                JOptionPane.showMessageDialog(null, nome + ", seu saldo é: R$ " + saldo);
+            } 
+           else if (opcao.equals("2")) {
+                String depositoStr = JOptionPane.showInputDialog("Digite o valor do depósito:");
+               deposito = Double.parseDouble(depositoStr);
+                saldo += deposito;
+                JOptionPane.showMessageDialog(null, "Depósito realizado com sucesso!");
+            } 
+           else if (opcao.equals("3")) {
+                String saqueStr = JOptionPane.showInputDialog("Digite o valor do saque:");
+                saque = Double.parseDouble(saqueStr);
+                saldo -= saque; 
+                JOptionPane.showMessageDialog(null, "Saque realizado com sucesso!");
+            } 
+           else if (opcao.equals("4")) {
+                JOptionPane.showMessageDialog(null, "Saindo do sistema. Até logo, " + nome + "!");
+                System.exit(0);
+               
+            } 
+           else {
+                JOptionPane.showMessageDialog(null, "Opção inválida! Tente novamente.");
+            }
+        }
+    }
+}
